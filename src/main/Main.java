0f1db8670dfdd37571b0305f13cb74a69e333de8
Cantox <alessandrocantoni3108@gmail.com>
2026-02-04 17:24:16 +0100

    Full restart

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,5 @@
 package main;
 
-import java.awt.Dimension;
-import javax.swing.JFrame;
-
-import libs.*;
-import pieces.*;
-import board.*;
-
-
 /**
  * Icons by: https://www.flaticon.com/authors/good-ware
  * 
@@ -15,15 +7,6 @@ import board.*;
  */
 public class Main {
       public static void main(String[] args) {
-              JFrame frame = new JFrame("Chess game");
-              frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
               
-              Board board = new Board(false);
-              board.setPreferredSize(new Dimension(Settings.ROWS * Settings.TILE_SIZE, Settings.COLS * Settings.TILE_SIZE));
-              
-              frame.add(board);
-              frame.pack();
-              frame.setResizable(false);
-              frame.setVisible(true);
       }
 }
